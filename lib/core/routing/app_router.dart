c95c1314f341fb5d26f272c2524689bd1217985f
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2025-02-06 21:51:45 +0200

    - added di - added AppRegex - integrated with login EP - added login repo & cubit - added login states with validation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,11 @@
+import 'package:doc_app/core/di/dependency_injection.dart';
 import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/features/home/ui/home_screen.dart';
+import 'package:doc_app/features/login/logic-cubit/login_cubit.dart';
 import 'package:doc_app/features/login/ui/login_screen.dart';
 import 'package:doc_app/features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -14,7 +18,13 @@ class AppRouter {
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
+          builder: (_) => BlocProvider(
+              create: (context) => getIt<LoginCubit>(),
+              child: const LoginScreen()),
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
         );
       default:
         return MaterialPageRoute(
