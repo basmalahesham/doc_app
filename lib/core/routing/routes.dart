e0fdc4e5f61c0570dc4d3ac185d9a0b0b18e25c0
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2025-02-07 01:06:23 +0200

    signup ui & api

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,4 @@
-class Routes{
+class Routes {
   static const String onBoardingScreen = "/onBoardingScreen";
   static const String loginScreen = "/loginScreen";
   static const String signUpScreen = '/signUpScreen';
